6460b01797bc3b055642cdece58d271d440a4a9f
MunKiJeon <jmk070994@gmail.com>
2024-06-27 12:54:09 +0900

    membershipform lv a

--- MembershipForm/app/src/main/java/com/example/membershipform/SignInActivity.kt
+++ MembershipForm/app/src/main/java/com/example/membershipform/SignInActivity.kt
@@ -15,19 +15,31 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignInActivity : AppCompatActivity() {
-
+    // Activity Result에 콜백 등록 (MainActivity.kt)
     private val getResultText = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ){ result ->
-        if (result.resultCode == Activity.RESULT_OK){
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
             resultDataId = result
-            et_signInId.setText(resultDataId!!.data!!.getStringExtra("ID") ?:throw Exception("아이디가 없습니다."))
+            et_signInId.setText(resultDataId!!.data!!.getStringExtra("ID") ?: throw Exception("아이디가 없습니다."))
             et_signInPw.setText("")
+            userData.add(
+                UserData(
+                    resultDataId!!.data!!.getStringExtra("ID") ?: throw Exception("No ID"),
+                    resultDataId!!.data!!.getStringExtra("PW") ?: throw Exception("No PW"),
+                    resultDataId!!.data!!.getStringExtra("NAME") ?: throw Exception("No Name"),
+                    resultDataId!!.data!!.getStringExtra("AGE") ?: throw Exception("No Age"),
+                    resultDataId!!.data!!.getStringExtra("MBTI") ?: throw Exception("No MBTI"),
+                    resultDataId!!.data!!.getStringExtra("GEN") ?: throw Exception("No Gender")
+                )
+            )
         }
     }
-    private var resultDataId : ActivityResult? = null
-    lateinit var et_signInId : EditText
-    lateinit var et_signInPw : EditText
+    private var resultDataId: ActivityResult? = null
+    private lateinit var et_signInId: EditText
+    private lateinit var et_signInPw: EditText
+
+    private var userData: MutableList<UserData> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +51,6 @@ class SignInActivity : AppCompatActivity() {
             insets
         }
 
-//        var et_signInId = findViewById<EditText>(R.id.et_sinId)//아이디 입력 필드
         et_signInId = findViewById<EditText>(R.id.et_sinId)//아이디 입력 필드
         et_signInPw = findViewById<EditText>(R.id.et_sinPw)//비밀번호 입력 필드
         var btn_sinSignIn = findViewById<Button>(R.id.btn_sinSignIn)//로그인 버튼
@@ -48,14 +59,27 @@ class SignInActivity : AppCompatActivity() {
         /*튜터님께 여쭤볼 사항
         1. 로고 안뜨는 이유 -> src 를 srcCompat으로 바꾸니 됨 왜지?? + 랜치 달린것과 아닌것 차이
         2. registerForActivityResult
-        3.
+        3. 개인프로필 클래스 생성후 넘기기...
         */
 
         btn_sinSignIn.setOnClickListener {
-            if (et_signInId.text.toString() != "" && et_signInPw.text.toString() != "") {
+            var loginCheck: Boolean = false
+            val it_Home = Intent(this, HomeActivity::class.java)
+            for (i in userData) {
+                if (i.id == et_signInId.text.toString() && i.pw == et_signInPw.text.toString()) {
+                    it_Home.putExtra("ID", i.id)
+                    it_Home.putExtra("PW", i.pw)
+                    it_Home.putExtra("NAME", i.name)
+                    it_Home.putExtra("AGE", i.age)
+                    it_Home.putExtra("GEN", i.gender)
+                    it_Home.putExtra("MBTI", i.mbti)
+                    loginCheck = true
+                    break
+                }
+            }
+
+            if (et_signInId.text.isNotEmpty() && et_signInPw.text.isNotEmpty() && loginCheck) {
                 Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show()
-                val it_Home = Intent(this, HomeActivity::class.java)
-                it_Home.putExtra("ID", et_signInId.text.toString())
                 startActivity(it_Home)
             } else {
                 Toast.makeText(this, "아이디/비밀번호를 확인해 주세요", Toast.LENGTH_SHORT).show()
@@ -67,11 +91,4 @@ class SignInActivity : AppCompatActivity() {
             getResultText.launch(it_SignUp)
         }
     }
-
-//    override fun onRestart() {
-//        super.onRestart()
-//        et_signInId.setText(resultDataId!!.data!!.getStringExtra("ID") ?:throw Exception("아이디가 없습니다."))//아이디 입력 필드
-//
-//    }
-
 }
--- MembershipForm/app/src/main/java/com/example/membershipform/SignupActivity.kt
+++ MembershipForm/app/src/main/java/com/example/membershipform/SignupActivity.kt
@@ -2,12 +2,17 @@ package com.example.membershipform
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.NumberPicker
+import android.widget.RadioButton
 import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +20,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignUpActivity : AppCompatActivity() {
-//개인프로필 클래스 생성후 넘기기...
+    private val numPick by lazy { findViewById<NumberPicker>(R.id.np_num) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,23 +31,63 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val nightModeFlags = //onCreate때 확인 가능함
+            getResources().configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
 
-        var iv_profilePic = findViewById<ImageView>(R.id.iv_profilePic)//프로필 사진
+        when (nightModeFlags) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                numPick.textColor = Color.WHITE
+            }
+
+            Configuration.UI_MODE_NIGHT_NO -> {
+                numPick.textColor = Color.WHITE
+            }
+        }
+        numPick.minValue = 14
+        numPick.maxValue = 90
+
         var et_supName = findViewById<EditText>(R.id.et_supName)//이름 입력 필드
+        var rb_supGenMale = findViewById<RadioButton>(R.id.rb_supGenMale)//남성 선택
+        var rb_supGenFemale = findViewById<RadioButton>(R.id.rb_supGenFemale)//여성 선택
+        var tb_supMbtiEI = findViewById<ToggleButton>(R.id.tb_supMbtiEI)//MBTI E-I
+        var tb_supMbtiSN = findViewById<ToggleButton>(R.id.tb_supMbtiSN)//MBTI S-N
+        var tb_supMbtiTF = findViewById<ToggleButton>(R.id.tb_supMbtiTF)//MBTI T-F
+        var tb_supMbtiJP = findViewById<ToggleButton>(R.id.tb_supMbtiJP)//MBTI J-P
+        var strMbti = "ESTJ"
         var et_supId = findViewById<EditText>(R.id.et_supId)//아이디 입력 필드
         var et_supPw = findViewById<EditText>(R.id.et_supPw)//비밀번호 입력 필드
         var btn_supSignUp = findViewById<Button>(R.id.btn_supSignUp)//회원가입 버튼
 
-//        btn_supSignUp.setOnClickListener {
-//            val intent = Intent(this,SignInActivity::class.java).apply {
-//                putExtra(SignInActivity.STRING_INTENT_KEY, "Returning to Main Activity")
-//            }
-//            setResult(Activity.RESULT_OK, et_supId.text)
-//            if(!isFinishing) finish()
-//        }
+        tb_supMbtiEI.setOnClickListener {
+            if(tb_supMbtiEI.isChecked)strMbti = strMbti.replace("E","I")
+            else strMbti = strMbti.replace("I","E")
+            Log.d("MBTI","MBTI:${strMbti}")
+        }
+        tb_supMbtiSN.setOnClickListener {
+            if(tb_supMbtiSN.isChecked)strMbti = strMbti.replace("S","N")
+            else strMbti = strMbti.replace("N","S")
+            Log.d("MBTI","MBTI:${strMbti}")
+        }
+        tb_supMbtiTF.setOnClickListener {
+            if(tb_supMbtiTF.isChecked)strMbti = strMbti.replace("T","F")
+            else strMbti = strMbti.replace("F","T")
+            Log.d("MBTI","MBTI:${strMbti}")
+        }
+        tb_supMbtiJP.setOnClickListener {
+            if(tb_supMbtiJP.isChecked)strMbti = strMbti.replace("J","P")
+            else strMbti = strMbti.replace("P","J")
+            Log.d("MBTI","MBTI:${strMbti}")
+        }
 
         btn_supSignUp.setOnClickListener {
-            if (et_supName.text.toString() != "" && et_supId.text.toString() != "" && et_supPw.text.toString() != "") {
+
+            var supRegex = et_supId.text.matches(Regex("^[\\w]{4,15}\$")) //(숫자, 문자 포함의 4~15자리 이내)
+                && et_supPw.text.matches(Regex("^[\\w!@#$%^&*~`\\-_=+]{6,15}\$"))
+                //(숫자, 문자, 특수문자 포함 6~15자리 이내)
+
+            var supTure = (et_supName.text.isNotEmpty() && et_supId.text.isNotEmpty() && et_supPw.text.isNotEmpty()
+                            && (rb_supGenMale.isChecked || rb_supGenFemale.isChecked ))
+            if (supTure && supRegex) {
                 Log.d(
                     "디버깅_회원가입_if",
                     "NAME:${et_supName.text} ID:${et_supId.text} PW: ${et_supPw.text}"
@@ -49,13 +95,19 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "${et_supName.text}님, 환영합니다!", Toast.LENGTH_SHORT).show()
 
                 val intent = Intent(this, SignInActivity::class.java).apply {
-                    putExtra("ID", "${et_supId.text}")
+                    putExtra("ID",  "${et_supId.text}")
+                    putExtra("PW",  "${et_supPw.text}")
+                    putExtra("NAME","${et_supName.text}")
+                    putExtra("AGE", "${numPick.value}")
+                    if (rb_supGenMale.isChecked) putExtra("GEN", "${rb_supGenMale.text}")
+                    else putExtra("GEN", "${rb_supGenFemale.text}")
+                    putExtra("MBTI", "$strMbti")
                 }
                 setResult(Activity.RESULT_OK, intent)
                 finish()
-//                val intent = Intent(this,SignInActivity::class.java )
-//                startActivity(intent)
-            } else {
+            } else if(!supRegex){
+                Toast.makeText(this, "입력 양식을 지켜주세요. \nID = 숫자, 문자 포함의 4~15자리 이내\nPW = 숫자, 문자, 특수문자 포함의 6~15자리 이내", Toast.LENGTH_SHORT).show()
+            }else {
                 Log.d(
                     "디버깅_회원가입_else",
                     "NAME:${et_supName.text} ID:${et_supId.text} PW: ${et_supPw.text}"
